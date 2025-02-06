77a10ddddbbb0bb8f8d0ef306d9f8530b5d06845
Matthew Ivancic <matthew.ivancic91@gmail.com>
2025-02-06 13:54:42 -0600

    refactored stack.h to utilize arrays instead of vectors

--- HW5-1.cpp
+++ HW5-1.cpp
@@ -4,8 +4,7 @@
 #include <sstream>
 
 //This creates a mutex object for std::cout
-//This is necessary to ensure that all 3 threads do not write to std::cout at the same time and create garbled output
-//This is also created outside of either function to ensure that all threads can access it
+//This is created outside of either function to ensure that all threads can access it
 std::mutex coutMutex;
 
 
